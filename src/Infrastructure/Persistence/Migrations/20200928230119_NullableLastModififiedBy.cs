a2cc3222a416ca37122d133a4834099d3a2933b1
Jesse Bellingham <jesse.bellingham@xero.com>
2020-09-29 09:02:21 +1000

    Nullable last modified by

---
+++ src/Infrastructure/Persistence/Migrations/20200928230119_NullableLastModififiedBy.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SharedPhotoAlbum.Infrastructure.Persistence.Migrations
+{
+    public partial class NullableLastModififiedBy : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<Guid>(
+                name: "LastModifiedBy",
+                table: "StoredMedia",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uuid");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "LastModifiedBy",
+                table: "Posts",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uuid");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "LastModifiedBy",
+                table: "Feeds",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uuid");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "LastModifiedBy",
+                table: "Comments",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uuid");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<Guid>(
+                name: "LastModifiedBy",
+                table: "StoredMedia",
+                type: "uuid",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "LastModifiedBy",
+                table: "Posts",
+                type: "uuid",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "LastModifiedBy",
+                table: "Feeds",
+                type: "uuid",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "LastModifiedBy",
+                table: "Comments",
+                type: "uuid",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldNullable: true);
+        }
+    }
+}
