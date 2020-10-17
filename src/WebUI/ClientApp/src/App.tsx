038ece479bf1ae499047442cb6ff80a13b765008
Jesse Bellingham <jesse.bellingham@xero.com>
2020-10-17 20:29:08 +1100

    Fix spa build

--- src/WebUI/ClientApp/src/App.tsx
+++ src/WebUI/ClientApp/src/App.tsx
@@ -1,10 +1,8 @@
 import React from 'react'
 import Feed from './components/Feed'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Switch } from 'react-router-dom'
 import { CloudinaryContext } from 'cloudinary-react'
 import Invite from './components/Invite'
-import { ApplicationPaths } from './components/api-authorization/ApiAuthorizationConstants'
-import ApiAuthorizationRoutes from './components/api-authorization/ApiAuthorizationRoutes'
 import { store, StoreContext } from './stores/StoreContext'
 import AuthorizeRoute from './components/api-authorization/AuthorizeRoute'
 
@@ -16,7 +14,6 @@ function App() {
             <StoreContext.Provider value={store}>
                 <Router>
                     <Switch>
-                        <Route path={ApplicationPaths.ApiAuthorizationPrefix} component={ApiAuthorizationRoutes} />
                         <AuthorizeRoute path="/invite/:inviteCode" component={Invite} />
                         <AuthorizeRoute path="/:feedId" component={Feed} />
                         <AuthorizeRoute path="/" component={Feed} />
